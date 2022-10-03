a38d863a77c3185d00b6abe6be26ebd012258bb3
Tab-Y <tlyoumans@gmail.com>
2022-10-03 19:02:52 -0400

    html and css structure

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,3 +1,7 @@
 // scores for the board (Position in array)(user input Name)(score)
 // array needs to sort by score from highest to lowest
-// 
+// time needs to count down and track as score - start at 120 seconds (120000ms)
+// score drops 100 points per second (120 * 100 = 12000 score max)
+// quiz container changes during quiz
+// prompts on screen 5-10 questions
+// need array for localstorage
