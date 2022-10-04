d66d0e732adf4bb6f160789915743070dc34658c
Tab-Y <tlyoumans@gmail.com>
2022-10-04 19:55:56 -0400

    coding updating

--- assets/JavaScript/highscores.js
+++ assets/JavaScript/highscores.js
@@ -1,2 +1,46 @@
 // code got storing highscores in local storage
 // primary code for high-scores page
+var highscoresList = document.querySelector("#theList")
+// render highscores
+// displays scores in elements
+// clear highscores
+// clears local storage to empty display
+
+
+
+function highscoreDisplay() {
+    highscoresRecall = JSON.parse(localStorage.getItem("userHighscore"));
+    userName.textContent = highscoresRecall.userName;
+    userScore.textContent = highscoresRecall.userScore;
+};
+
+function renderScores() {
+
+
+    for (var i = 0; i < 10; i++) {
+        var score = highscores[i];
+
+        var li = document.createElement("li");
+        li.textContent = userName, score;
+        li.setAttribute("data-index", i);
+
+        
+        highscoresList.appendChild(li);
+
+        // create list element
+        // put user name and their score into list
+        // append list element
+    }
+};
+
+submitButton.addEventListener("click", function(event){
+    event.preventDefault();
+    var highscore = {
+        userName: userNameInput.value,
+        userScore: timeRemaining.value,
+    };
+    localStorage.setItem("userHighscore", JSON.stringify(highscore));
+});
+
+
+renderScores();
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,3 +1,33 @@
+// Array for keeping questions and answer organized
+var questions = [
+    {
+        question: "What is a boolean?",
+        choices: ["A true or false statement", "A group of words stored together", "another answer", "clearly a wrong answer"],
+        answer: "A true or false statement",
+    },
+    {
+        question: "Question 2",
+        choices: ["A", "B", "C", "D"],
+        answer: "B",
+    },
+    {
+        question: "Question 3",
+        choices: ["A", "B", "C", "D"],
+        answer: "C",
+    },
+    {
+        question: "Question 4",
+        choices: ["A", "B", "C", "D"],
+        answer: "D",
+    },
+    {
+        question: "Question 5",
+        choices: ["A", "B", "C", "D"],
+        answer: "A",
+    },
+];
+
+
 // scores for the board (Position in array)(user input Name)(score)
 // array needs to sort by score from highest to lowest
 // time needs to count down and track as score - start at 120 seconds (120000ms)
@@ -5,6 +35,8 @@
 // quiz container changes during quiz
 // prompts on screen 5-10 questions
 // need array for localstorage
+
+
 
 
 // var container = document.querySelector(".container");
@@ -36,20 +68,79 @@ var wrongEl = document.querySelector(".wrong");
 var quizSiteEl = document.querySelector(".quizSite");
 var instructionHeadEl = document.querySelector(".instructionHead");
 var instructionTextEl = document.querySelector(".instructionText");
+var headerEl = document.querySelector(".header");
+var questionsEl = document.querySelector("#questions");
+var questionsBoxEl = document.querySelector('.questionsBox');
+var submitButtonEl = document.querySelector(".submitButton");
+var answerButtonEl = document.querySelector(".answer-buttons");
+// dynamicly coded in var answerButtonEl = document.querySelector(".answersButton")
+// runs function to check if answer is right on click
+// displays on screen if correct or wrong
+// sets delay of 500 to show next question
+
+function generateQuestion(){
+    
+    for (var i = 0; i < questions.length; i++) {
+        // make the question
+        questionsEl.textContent = questions[i].question;
+            // make buttons
+            var makeButtonEl = document.createElement('button');
+            makeButtonEl.classList.add('answers');
+            makeButtonEl.textContent = questions[i].choices;
+            answerButtonEl.appendChild(makeButtonEl);
+
+    }
+    }
+    // compare the answer
+//     for (var i = 0; i < questions.length; i++) {
+//         questionsEl.textContent = questions[i].question;
+
+  
+//         questions[i].choices.forEach(choice => {
+//             if (questions.choices === questions.answer) {
+//                 correct();
+//             } else {
+//                 wrong();
+//             }
+//             button.addEventListener('click', selectAnswer)
+//         })
+//        }
+
+
+// function nextQuestion(){
+//     questionsEl.textContent = questions.question;
+//     questions.choices.forEach(choices => {
+//         const button = document.createElement('button')
+//     });
+
+// }
 
 
 function gameOver() {
+    alert("Game over!")
     console.log("game over")
 }
-function correct() {
 
-}
+// function saveScore() {
+//     var userInfo = {
+//         userName: userNameInput.value,
+//         userScore: timeLeft.value,
+//     };
+//     localStorage.setItem("userHighscore", JSON.stringify(highscore));
+// }
 
-function wrong () {
-    timeLeft - 10;
+// function correct() {
+//     // flash correct
+//     nextQuestion();
+// }
 
-}
+// function wrong () {
+//     // flash wrong
+//     timeLeft + -10;
+//     nextQuestion();
 
+// }
+// working from here down
 function countdown() {
     console.log("works 1")
     var timeLeft = 60;
@@ -59,34 +150,46 @@ function countdown() {
 
     if (timeLeft === 0) {
         clearInterval(timeInterval);
+        timeLeft = '';
         gameOver();
     }
     }, 1000);
 } 
 
 function startGame() {
-    console.log("works 1");
-
-    var timeLeft = 60;
-    var timeInterval = setInterval(function () {
-        timeLeft--;
-        timerEl.textContent = "Time remaining " + timeLeft;
-
-    if (timeLeft === 0) {
-        clearInterval(timeInterval);
-        gameOver();
-    }
-    }, 1000);
-    console.log("working");
- 
-}
+    console.log("started");
+    quizSiteEl.classList.add("hide");
+    // remove hide class to questions
+    questionsBoxEl.classList.remove("hide");
+    countdown();
+    generateQuestion();
+};
+// working from here up
 
 
+// function checkAnswer(){
+//     if (questions.choices === questions.answer){
+//         correct();
+//     } else {
+//         wrong();
+//     }
+// };
+
+// submitButton.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var highscore = {
+//         userName: userNameInput.value,
+//         userScore: timeRemaining.value,
+//     };
+//     localStorage.setItem("userHighscore", JSON.stringify(highscore));
+// });
 
 
 
 
 
 quizEl.addEventListener("click", startGame);
+// nextButtonEl.addEventListener("click", checkAnswer);
+// submitButtonEl.addEventListener("click", saveScore);
 // correctEl.addEventListener("click", correct)
 // wrongEl.addEventListener("click", wrong)
