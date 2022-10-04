7675bc50a470b5b017e835a83716c30f0d07665d
Tab-Y <tlyoumans@gmail.com>
2022-10-03 21:11:58 -0400

    html and css adjustments

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -5,3 +5,88 @@
 // quiz container changes during quiz
 // prompts on screen 5-10 questions
 // need array for localstorage
+
+
+// var container = document.querySelector(".container");
+
+// container.addEventListener("click", function(event) {
+//   var element = event.target;
+
+//   // TODO: Complete function
+//   if (element.matches(".box")) {
+//     var state = element.getAttribute("data-state");
+    
+//     if (state === "hidden") {
+//       element.textContent = element.dataset.number
+//       element.dataset.state = "visible";
+      
+      
+//     } else {
+//       element.textContent = ""
+//       element.dataset.state = "hidden";
+
+//     }
+//   }
+// });
+
+var timerEl = document.getElementById('time');
+var quizEl = document.getElementById('quiz');
+var correctEl = document.querySelector(".correct");
+var wrongEl = document.querySelector(".wrong");
+var quizSiteEl = document.querySelector(".quizSite");
+var instructionHeadEl = document.querySelector(".instructionHead");
+var instructionTextEl = document.querySelector(".instructionText");
+
+
+function gameOver() {
+    console.log("game over")
+}
+function correct() {
+
+}
+
+function wrong () {
+    timeLeft - 10;
+
+}
+
+function countdown() {
+    console.log("works 1")
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = "Time remaining " + timeLeft;
+
+    if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        gameOver();
+    }
+    }, 1000);
+} 
+
+function startGame() {
+    console.log("works 1");
+
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = "Time remaining " + timeLeft;
+
+    if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        gameOver();
+    }
+    }, 1000);
+    console.log("working");
+ 
+}
+
+
+
+
+
+
+
+quizEl.addEventListener("click", startGame);
+// correctEl.addEventListener("click", correct)
+// wrongEl.addEventListener("click", wrong)
